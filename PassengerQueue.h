43557f362be14c3cab3a21918a522333d8b03c06
oelmady <oelmady@gmail.com>
2022-06-19 01:35:12 -0400

    pq

--- PassengerQueue.h
+++ PassengerQueue.h
@@ -8,20 +8,39 @@ The PassengerQueue class implements the functionality for the metro simulator, a
 
 #include <iostream>
 #include <string>
+#include <list>
 
 #include "Passenger.h"
-
-using namespace std;
 
 class PassengerQueue
 {
 public:
-    Passenger front();
-    void dequeue();
-    void enqueue(const Passenger &p);
-    int size();
-    void print(ostream &output);
+    Passenger front() { return queue.front(); }
+    void dequeue() 
+    { 
+        if (!queue.empty()) 
+        { 
+            queue.pop_front(); 
+        }
+    }
+    void enqueue(const Passenger &p) 
+    { 
+        queue.push_back(p); 
+    }
+    int size() 
+    { 
+        return queue.size(); 
+    }
+    void print(std::ostream &os) 
+    {
+        for (const Passenger &current : queue)
+        {
+            string out = "[" + to_string(current.id) + ", " + to_string(current.from) + "->" + to_string(current.to) + "]";
+            os << out << std::end;
+        } // strange bug: expected ; ??
+    }
 private:
+    std::list<Passenger> queue;
+};
 
-}
-#endif
+#endif;
